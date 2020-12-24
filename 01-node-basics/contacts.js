c8366572061df91c626af6caa761a9d93a2d4ee0
kovalchukaiew <kovalchukaiew@gmail/com>
2020-12-24 19:17:24 +0200

    push

---
+++ 01-node-basics/contacts.js
@@ -0,0 +1,28 @@
+const fs = require ('fs');
+// const { dirname } = require('path');
+
+const path = require ('path');
+
+const contactsPath = path.join(__dirname, './dp/contacts.json');
+
+console.log(contactsPath);
+
+
+
+
+// //  задокументировать каждую функцию
+// function listContacts() {
+//     // ...твой код
+//   }
+  
+//   function getContactById(contactId) {
+//     // ...твой код
+//   }
+  
+//   function removeContact(contactId) {
+//     // ...твой код
+//   }
+  
+//   function addContact(name, email, phone) {
+//     // ...твой код
+//   }
