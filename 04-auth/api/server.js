d0994b75768a389294a3fe71c047b51ceddcb55b
kovalchukaiew <kovalchukaiew@gmail/com>
2021-02-16 15:23:24 +0200

    04-auth

---
+++ 04-auth/api/server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const cors = require('cors');
+require('dotenv').config();
+
+const contactRouter = require('./contacts/contact.router');
+const userRouter = require('./users/user.router');
+
+const accessLogStream = require('../utils/accessLogStream');
+
+class ContactsServer {
+		constructor() {
+		this.server = null;
+		this.port = 8080;
+	}
+
+		async start() {
+		this.initServer();
+		this.initMiddleware();
+		this.initRoutes();
+		await this.initDatabase();
+		this.startListening();
+	}
+
+		initServer() {
+		this.server = express();
+	}
+
+		initMiddleware() {
+		this.server.use(express.json());
+		this.server.use(morgan('combined', { stream: accessLogStream }));
+		this.server.use(cors({ origin: 'http://localhost:3000' }));
+	}
+
+		initRoutes() {
+		this.server.use('/api/auth', userRouter);
+		this.server.use('/api/contacts', contactRouter);
+		this.server.use('/api/users', userRouter);
+	}
+
+		async initDatabase() {
+		try {
+			await mongoose.connect(process.env.MONGODB_URL, {
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useFindAndModify: false,
+			});
+
+			console.log('Database connection successful');
+		} catch (error) {
+			console.log(`MongoDB error: ${error.message}`);
+			process.exit(1);
+		}
+	}
+
+		startListening() {
+		this.server.listen(this.port, () => {
+			console.log('Server started listening on port', this.port);
+		});
+	}
+}
+
+module.exports = ContactsServer;
---
+++ 04-auth/api/users/user.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+
+const { subscriptionEnum } = require('../../configs/configs').users;
+
+const userSchema = new Schema({
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+	subscription: {
+		type: String,
+		enum: subscriptionEnum,
+		required: false,
+		default: 'free',
+	},
+	token: { type: String, required: false },
+});
+
+module.exports = model('User', userSchema);
